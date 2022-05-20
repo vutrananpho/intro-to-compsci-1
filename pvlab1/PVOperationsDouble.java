2fd9ddfc3174594e5dd5f555f535d4f52f877927
Pho (Tran An) Vu / Haemin <phovu0703@gmail.com>
2022-05-20 19:21:22 -0400

    Add files via upload

---
+++ pvlab1/PVOperationsDouble.java
@@ -0,0 +1,16 @@
+/** File/Week1/Lab1/Hello/PVOperationsDouble.java  @PV Pho Vu
+ * You have 2 doubles and perform math operations: +, -, *,
+ * #5 in Lab
+ */
+
+public class PVOperationsDouble { // start class
+    public static void main(String[] args) { // start main
+        double e, double f; // declare variables
+        e = 34; // set e = 34
+        f = 55; // set f = 3
+        System.out.println("e + f = " + (e + f)); // display addition
+        System.out.println("e - f = " + (e - f)); // display subtraction
+        System.out.println("e * f = " + (e * f)); // display multiplication
+
+    } // end main
+} // end class
---
+++ pvlab1/PVnCube.java
@@ -0,0 +1,8 @@
+/** File/Week1/Lab1/Hello/PVnCube.java @PV Pho Vu
+ */
+public class PVnCube { public static void main(String[] args) {
+        int n = (int)(Math.random()*1001);
+        System.out.println(n);
+        System.out.println(Math.pow(n,3));
+    } // end main
+} // end class
