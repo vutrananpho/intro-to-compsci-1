72fdf86133e34f65e7e33ab810bdc6f7cd995814
Pho (Tran An) Vu / Haemin <phovu0703@gmail.com>
2022-05-20 19:19:55 -0400

    Delete java/lab directory

--- java/lab/lab1sol/nCube.java
+++
@@ -1,8 +0,0 @@
-/** File/Week1/Lab1/Hello/nCube.java Author @PV Pho Vu
- */
-public class nCube { public static void main(String[] args) {
-        int n = (int)(Math.random()*1001;
-        System.out.println(n);
-        System.out.println(Math.pow(n,3));
-    } // end main
-} // end class
